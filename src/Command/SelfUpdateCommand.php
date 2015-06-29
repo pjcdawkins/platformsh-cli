f9acf8a06b9ddacab69f1021a058fd1b8b9950d4
Patrick Dawkins <pjcdawkins@gmail.com>
2015-06-29 20:55:55 +0100

    Use padraic/phar-updater

--- src/Command/SelfUpdateCommand.php
+++ src/Command/SelfUpdateCommand.php
@@ -2,8 +2,7 @@
 
 namespace Platformsh\Cli\Command;
 
-use Herrera\Phar\Update\Manager;
-use Herrera\Phar\Update\Manifest;
+use Humbug\SelfUpdate\Updater;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -16,7 +15,8 @@ class SelfUpdateCommand extends PlatformCommand
           ->setName('self-update')
           ->setAliases(array('up'))
           ->setDescription('Update the CLI to the latest version')
-          ->addOption('manifest', null, InputOption::VALUE_OPTIONAL, 'The manifest file location');
+          ->addOption('manifest', null, InputOption::VALUE_OPTIONAL, 'The manifest file location')
+          ->addOption('no-key', null, InputOption::VALUE_NONE, 'Skip key verification');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -25,10 +25,25 @@ class SelfUpdateCommand extends PlatformCommand
 
         $currentVersion = $this->getApplication()->getVersion();
 
-        $manager = new Manager(Manifest::loadFile($manifest));
-        $updated = $manager->update($this->getApplication()->getVersion(), true);
+        $localPhar = \Phar::running(false);
+        if (!$localPhar) {
+            $this->stdErr->writeln('This instance of the CLI was not installed as a Phar archive.');
+            return 1;
+        }
+
+        // Use the GitHub / Packagist strategy.
+        $hasPubKey = !$input->getOption('no-key');
+        $updater = new Updater($localPhar, $hasPubKey);
+        $updater->setStrategy(Updater::STRATEGY_GITHUB);
+        /** @var \Humbug\SelfUpdate\Strategy\GithubStrategy $strategy */
+        $strategy = $updater->getStrategy();
+        $strategy->setPackageName('platformsh/cli');
+        $strategy->setPharName('platform.phar');
+        $strategy->setCurrentLocalVersion($currentVersion);
+
+        $updated = $updater->update();
         if ($updated) {
-            $newVersion = $this->getApplication()->getVersion();
+            $newVersion = $updater->getNewVersion();
             $this->stdErr->writeln("Successfully updated from <info>$currentVersion</info> to <info>$newVersion</info>");
         }
         else {
