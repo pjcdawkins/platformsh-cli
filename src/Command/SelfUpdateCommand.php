cb3c2aa36214a28f73be42ae56ee9a57014771da
Patrick Dawkins <pjcdawkins@gmail.com>
2015-06-29 08:55:47 +0100

    Improve self-update command

--- src/Command/SelfUpdateCommand.php
+++ src/Command/SelfUpdateCommand.php
@@ -4,18 +4,18 @@ namespace Platformsh\Cli\Command;
 
 use Herrera\Phar\Update\Manager;
 use Herrera\Phar\Update\Manifest;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class SelfUpdateCommand extends Command
+class SelfUpdateCommand extends PlatformCommand
 {
     protected function configure()
     {
         $this
           ->setName('self-update')
-          ->setDescription('Updates platform.phar to the latest version')
+          ->setAliases(array('up'))
+          ->setDescription('Update the CLI to the latest version')
           ->addOption('manifest', null, InputOption::VALUE_OPTIONAL, 'The manifest file location');
     }
 
@@ -23,7 +23,16 @@ class SelfUpdateCommand extends Command
     {
         $manifest = $input->getOption('manifest') ?: 'https://platform.sh/cli/manifest.json';
 
+        $currentVersion = $this->getApplication()->getVersion();
+
         $manager = new Manager(Manifest::loadFile($manifest));
-        $manager->update($this->getApplication()->getVersion(), true);
+        $updated = $manager->update($this->getApplication()->getVersion(), true);
+        if ($updated) {
+            $newVersion = $this->getApplication()->getVersion();
+            $this->stdErr->writeln("Successfully updated from <info>$currentVersion</info> to <info>$newVersion</info>");
+        }
+        else {
+            $this->stdErr->writeln("No updates found. The Platform.sh CLI is up-to-date.");
+        }
     }
 }
